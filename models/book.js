f2039423e17e9656c7536362beddec753a0beb6d
irfanca1 <irfanchoiruddin23@gmail.com>
2023-06-09 18:36:11 +0700

    install package

---
+++ models/book.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const Book = sequelize.define('Book', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+});
+
+module.exports = Book;
