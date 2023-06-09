ceb930779e6f4f83520ba044d96111a743792137
irfanca1 <irfanchoiruddin23@gmail.com>
2023-06-09 19:07:27 +0700

    install package

--- models/book.js
+++ models/book.js
@@ -1,24 +1,28 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../database');
+
+module.exports = () => {
   class Book extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      // define associations here
     }
   }
-  Book.init({
-    name: DataTypes.STRING,
-    price: DataTypes.DECIMAL
-  }, {
-    sequelize,
-    modelName: 'Book',
-  });
+
+  Book.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Book',
+    }
+  );
   return Book;
 };
